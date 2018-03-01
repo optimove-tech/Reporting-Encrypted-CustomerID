3185078533d9c2c1c211ee3ad5e2c08503498519
Optimove Product Integration <36954231+optimoveproductintegration@users.noreply.github.com>
2018-03-01 12:53:18 +0200

    Add files via upload
    added .Net

---
+++ SDKEncryption/SDKEncryption/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        public static class AESThenHMAC
+        {
+            private static readonly RandomNumberGenerator Random = RandomNumberGenerator.Create();
+
+            public static readonly int BlockBitSize = 128;
+            public static readonly int KeyBitSize = 256;
+            public static readonly int Iterations = 10000;
+
+            public static byte[] NewKey()
+            {
+                var key = new byte[KeyBitSize / 8];
+                Random.GetBytes(key);
+                return key;
+            }
+
+            public static string Encrypt(string secretMessage, byte[] cryptKey, byte[] authKey,
+                                               byte[] nonSecretPayload = null)
+            {
+                if (string.IsNullOrEmpty(secretMessage))
+                    throw new ArgumentException("Secret Message Required!", "secretMessage");
+
+                var plainText = Encoding.UTF8.GetBytes(secretMessage);
+                var cipherText = SimpleEncrypt(plainText, cryptKey, authKey, nonSecretPayload);
+                return Convert.ToBase64String(cipherText);
+            }
+            public static string Decrypt(string encryptedMessage, byte[] cryptKey, byte[] authKey,
+                                               int nonSecretPayloadLength = 0)
+            {
+                if (string.IsNullOrWhiteSpace(encryptedMessage))
+                    throw new ArgumentException("Encrypted Message Required!", "encryptedMessage");
+
+                var cipherText = Convert.FromBase64String(encryptedMessage);
+                var plainText = SimpleDecrypt(cipherText, cryptKey, authKey, nonSecretPayloadLength);
+                return plainText == null ? null : Encoding.UTF8.GetString(plainText);
+            }
+            public static byte[] SimpleEncrypt(byte[] secretMessage, byte[] cryptKey, byte[] authKey, byte[] nonSecretPayload = null)
+            {
+                if (cryptKey == null || cryptKey.Length != KeyBitSize / 8)
+                    throw new ArgumentException(String.Format("Key needs to be {0} bit!", KeyBitSize), "cryptKey");
+
+                if (authKey == null || authKey.Length != KeyBitSize / 8)
+                    throw new ArgumentException(String.Format("Key needs to be {0} bit!", KeyBitSize), "authKey");
+
+                if (secretMessage == null || secretMessage.Length < 1)
+                    throw new ArgumentException("Secret Message Required!", "secretMessage");
+
+                nonSecretPayload = nonSecretPayload ?? new byte[] { };
+
+                byte[] cipherText;
+                byte[] iv;
+
+                using (var aes = new AesManaged
+                {
+                    KeySize = KeyBitSize,
+                    BlockSize = BlockBitSize,
+                    Mode = CipherMode.CBC,
+                    Padding = PaddingMode.PKCS7
+                })
+                {
+                    aes.GenerateIV();
+                    iv = aes.IV;
+                    using (var encrypter = aes.CreateEncryptor(cryptKey, iv))
+                    using (var cipherStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(cipherStream, encrypter, CryptoStreamMode.Write))
+                        using (var binaryWriter = new BinaryWriter(cryptoStream))
+                        {
+                            binaryWriter.Write(secretMessage);
+                        }
+
+                        cipherText = cipherStream.ToArray();
+                    }
+                }
+
+                using (var hmac = new HMACSHA256(authKey))
+                using (var encryptedStream = new MemoryStream())
+                {
+                    using (var binaryWriter = new BinaryWriter(encryptedStream))
+                    {
+                        binaryWriter.Write(nonSecretPayload);
+                        binaryWriter.Write(iv);
+                        binaryWriter.Write(cipherText);
+                        binaryWriter.Flush();
+                        var tag = hmac.ComputeHash(encryptedStream.ToArray());
+                        binaryWriter.Write(tag);
+                    }
+
+                    return encryptedStream.ToArray();
+                }
+            }
+            public static byte[] SimpleDecrypt(byte[] encryptedMessage, byte[] cryptKey, byte[] authKey, int nonSecretPayloadLength = 0)
+            {
+                //Basic Usage Error Checks
+                if (cryptKey == null || cryptKey.Length != KeyBitSize / 8)
+                    throw new ArgumentException(String.Format("CryptKey needs to be {0} bit!", KeyBitSize), "cryptKey");
+
+                if (authKey == null || authKey.Length != KeyBitSize / 8)
+                    throw new ArgumentException(String.Format("AuthKey needs to be {0} bit!", KeyBitSize), "authKey");
+
+                if (encryptedMessage == null || encryptedMessage.Length == 0)
+                    throw new ArgumentException("Encrypted Message Required!", "encryptedMessage");
+
+                using (var hmac = new HMACSHA256(authKey))
+                {
+                    var sentTag = new byte[hmac.HashSize / 8];
+                    //Calculate Tag
+                    var calcTag = hmac.ComputeHash(encryptedMessage, 0, encryptedMessage.Length - sentTag.Length);
+                    var ivLength = (BlockBitSize / 8);
+                    //if message length is to small just return null
+                    if (encryptedMessage.Length < sentTag.Length + nonSecretPayloadLength + ivLength)
+                        return null;
+                    //Grab Sent Tag
+                    Array.Copy(encryptedMessage, encryptedMessage.Length - sentTag.Length, sentTag, 0, sentTag.Length);
+                    //Compare Tag with constant time comparison
+                    var compare = 0;
+                    for (var i = 0; i < sentTag.Length; i++)
+                        compare |= sentTag[i] ^ calcTag[i];
+                    //if message doesn't authenticate return null
+                    if (compare != 0)
+                        return null;
+
+                    using (var aes = new AesManaged
+                    {
+                        KeySize = KeyBitSize,
+                        BlockSize = BlockBitSize,
+                        Mode = CipherMode.CBC,
+                        Padding = PaddingMode.PKCS7
+                    })
+                    {
+                        //Grab IV from message
+                        var iv = new byte[ivLength];
+                        Array.Copy(encryptedMessage, nonSecretPayloadLength, iv, 0, iv.Length);
+                        using (var decrypter = aes.CreateDecryptor(cryptKey, iv))
+                        using (var plainTextStream = new MemoryStream())
+                        {
+                            using (var decrypterStream = new CryptoStream(plainTextStream, decrypter, CryptoStreamMode.Write))
+                            using (var binaryWriter = new BinaryWriter(decrypterStream))
+                            {
+                                //Decrypt Cipher Text from Message
+                                binaryWriter.Write(
+                                    encryptedMessage,
+                                    nonSecretPayloadLength + iv.Length,
+                                    encryptedMessage.Length - nonSecretPayloadLength - iv.Length - sentTag.Length
+                                );
+                            }
+
+                            return plainTextStream.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string secretMessage = "secret message";
+            var key = Encoding.UTF8.GetBytes("DH6asttV1CL2yp6YaXPimFSHc9BM3xiw");
+            var encryptedMessage = AESThenHMAC.Encrypt(secretMessage, key, key);
+            var decrypted = AESThenHMAC.Decrypt(encryptedMessage, key, key);
+            Console.WriteLine("Encrypted Message: " + encryptedMessage);
+
+        }
+    }
+}
